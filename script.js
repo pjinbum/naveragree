b4f2b79624ca0c4209d732056b5439302bca8875
pjinbum <119594529+pjinbum@users.noreply.github.com>
2023-01-25 11:49:40 +0900

    🚀 go change

---
+++ script.js
@@ -0,0 +1,28 @@
+// $('.every-check').on('click' , function(){
+//   $('input').css({'background-color':'green'})
+// }
+// );
+
+
+// $('.every-check').off('click' , function(){
+//   $('input').css({'background-color':'none'})
+// }
+// );
+
+$('.every-check').on('click' , function(){
+     $('input').toggle(css({'background-color':'green'}))
+   }
+   );
+
+
+
+
+//체크박스 .every-check가 클릭되었을때
+//확인 버튼을 누르면 가입단계로 넘어간다
+
+
+let Next = document.querySelector('.success');
+
+Next.addEventListener('click', function(){
+
+})
