0267767bd4e0079527ee35aa3ec029d50a04606e
pjinbum <119594529+pjinbum@users.noreply.github.com>
2023-01-26 15:24:50 +0900

    🚑 모르겠다

--- 네이버동의정답/script.js
+++ 네이버동의정답/script.js
@@ -6,10 +6,50 @@ $('label').on('click' , function(e){
 
   if($(this).children('.checkbox-img').hasClass('checked')) {
     //.checkbox-img가 checked 클래스를 가지고 있을때 실행할 코드
-    $(this).children('input[type="checkbox"]').attr('checked')
+    $(this).children('input[type="checkbox"]').attr('checked' , 'checked')
   } else {
     //.checkbox-img가 checked 클래스를 가지고 있지 않을때 실행할 코드
-    $(this).children('input[type="checkbox"]').removeAttr('checked')
+    $(this).children('input[type="checkbox"]').removeAttr('checked' , 'checked')
 
   }
 })
+
+//total에 체크하면 모든.agree 체크
+//total에 언체크하면 모든.agree 언체크
+
+$('.total label').on('click' , function(){
+  if($(this).find('.checkbox-img').hasClass('checked')) {
+    // 모든 .agree 라는 클래스를 가진 것들이 체크 
+    $('.agree').find('.checkbox-img').addClass('checked');
+    $('.agree').find('input[type="checkbox"]').attr('checked','checked');
+    
+
+  } else {
+    $('.agree').find('.checkbox-img').removeClass('checked');
+    $('.agree').find('input[type="checkbox"]').removeAttr('checked','checked');
+  }
+});
+
+$('#cancel').on('click', function(){
+  location.href = "http://naver.com";
+});
+
+//#submit 버튼을 클릭 했을때 필수동의 사항에 모두 체크했는지 확인
+//모두 체크되어 있으면 통과
+//체크 안된게 있다면 #red-alert을 보여주고 전송을 중지
+
+$('#submit').on('click' , function(e){
+  let req = $('.req').length;
+  let chkreq = $('.req .checked').length;
+  let unchk =req - chkreq;
+  if(unchk == 0) {
+      $('#form1').submit();
+  } else {
+    // form이 제출되지 못하게 막기
+    e.preventDefault();
+    //#red-alert가 보이게
+    $('.red-alert').css({'visivility' : 'visible'});
+  }
+  
+})
+
