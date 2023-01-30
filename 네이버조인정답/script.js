b6aeed239de6603f22b9838dae8a8f28e90c3633
pjinbum <119594529+pjinbum@users.noreply.github.com>
2023-01-30 17:15:03 +0900

    🚀 CHANGE

--- 네이버조인정답/script.js
+++ 네이버조인정답/script.js
@@ -64,3 +64,58 @@ $('#userpw input').focusout(function(){
 
    }
 });
+
+//비밀번호 재확인
+//'#userpw-chk input 에서 포커스 아웃 됬을때 value값의 length가 0이라면 (조건1)
+//'#userpw-chk .warn에 빨간글씨로 필수정보 입니다. (실행문1)
+
+
+//그렇지 않다면 그것의 값과 #userpw input 안에 들어있는 값이 같은가? (조건2)
+//#userpw-chk .warn에 empty()로 비운다. (실행문1)
+//#userpw-chk .inputbox img의 속성중 src값을 변경한다. .
+// phoneveri 변수를 true로 변경 
+
+//그렇지 않다면 else
+//
+//
+$('#userpw-chk input').focusout(function(){
+  let userpwchk = $(this).val();
+  if(userpwchk.length==0){
+    $('#userpw-chk .warn').html('<span class="text-red">필수 정보입니다.</span>')
+    $('#userpw-chk .inputbox img').attr( 'src' , './image/m_icon_pw_step_02.png')
+
+
+  } else if (userpwchk == $('#userpw input').val()) {
+    pwchkveri = true;
+    //remove / empty
+    //remove 요소 자체를 지우고 empty 요소 안 내용을 지운다.
+    $('#userpw-chk .warn').empty();
+    $('#userpw-chk .inputbox img').attr('src' , './image/m_icon_pw_step_07.png');
+    
+  } else {
+    $('#userpw-chk .warn').html('<span class="text-red">비밀번호가 일치하지 않습니다</span>');
+    $('#userpw-chk .inputbox img').attr('src' , './image/m_icon_pw_step_02.png');
+  }
+});
+
+//이름
+//#username input 에서 focusout될때
+$('#username input').focusout(function(){
+  let username = $('#username input').val();
+  nameveri = false;
+  //js 정규 표현식 입력값을 체크
+  //형식 : /정규식/
+  //문자와 숫자가 아닌것을 걸러낼수 있음 ..여기서^표시는 부정의 표시
+  let reg = /[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]/g;
+  if(username.length == 0){
+    $('#username .warn').html('<span class="text-red">필수 정보 입니다.</span>');
+  } else if(reg.test(username)){
+    //정규식을 만족하면  true 만족하지 않으면 false 반환
+    $('#username .warn').html('<span class="text-red">한글과 영문 대 소문자를 사용하세요(특수기호 사용 불가)</span>');
+  } else {
+    nameveri = true;
+    $('#username .warn').html('<span class="text-green">잘하셨습니다(특수기호 사용 불가)</span>');
+    // $('#username .warn').empty();
+
+  }
+});
